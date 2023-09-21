d6e6b68010dd0f8ac7b17b21a28743e753e02768
Francesco85P <francescopaparazzo85@gmail.com>
2023-09-21 20:15:58 +0200

    Commit

--- Brffs_Shiny.R
+++ Brffs_Shiny.R
@@ -8,65 +8,59 @@ source("Variables_Shiny.R")
 
 # User interface ----
 ui <- fluidPage (
-  titlePanel("Diabetes association with demographic, physical and behavioural variables"),
+  titlePanel(h2("Diabetes association with demographic, physical and behavioural variables",
+             align = "center")),
+
   sidebarLayout(
-    sidebarPanel(
+    sidebarPanel(width=12,
       helpText("Create plots"),
       
-      selectInput("Mosaic", 
-                  label = "Choose a variable for the histogram plot",
-                  choices = c("Education_level","Income","employment_status","smoked_at_least_100_cigaretes"),
+      selectInput("Plot", 
+                  label = "Choose a variable for the  plot",
+                  choices = c("Education level" = "Education_level",
+                              "Sex", 
+                              "Income", 
+                              "Employment status" = "Employment_status",
+                              "Smoking",
+                              "BMI"),
                   selected = "Education_level"),
-      
-      selectInput("Boxplot", 
-                  label = "Choose a variable for the boxplot",
-                  choices = c("BMI","drinks_per_month","days_at_least_one_drink_last_month"),
-                  selected = "BMI"),
-      br(),
-      br(),
-      br()
-      
       
     ),
   
-    mainPanel(br(),
-              br(),
-      plotOutput("Plot1"),
-      br(),
-      plotOutput("Plot2"))
+    mainPanel(width=12,
+      plotOutput("Plot1", 
+                 )
   )
+),
+
 )
-
-
 server <- function(input, output){
   
-  output$Plot1 <- renderPlot({
-    
-    ggplot(data=brfss_diabetes_predict) + 
-      geom_mosaic(aes(x = product(diabetes,!!sym(input$Mosaic)), fill=!!sym(input$Mosaic))) +   
-      theme_classic()+
-      labs(title = paste0("Diabestes and ", input$Mosaic))+
-      theme(plot.title = element_text(hjust = 0.5))+
-      theme(axis.title.x=element_blank(),
-            axis.text.x=element_blank(),
-            axis.ticks.x=element_blank())
   
-       })
-    
-    
-  output$Plot2 <- renderPlot({   
-    
-  ggplot(data=brfss_diabetes_predict, aes(x=diabetes,y=log(get(input$Boxplot))))+
+  output$Plot1 <- renderPlot({
+    if (input$Plot %in% c ("Education_level","Income","Employment_status", "Sex",
+      "Smoking")){
+        ggplot(data=brfss_diabetes_predict) + 
+        geom_mosaic(aes(x = product(diabetes,!!sym(input$Plot)), fill=!!sym(input$Plot))) +   
+        theme_classic()+
+        labs(y="Diabetes", x= input$Plot, title = paste0("Diabetes and ",  gsub("\\_", " ", input$Plot )))+
+        theme(plot.title = element_text(hjust = 0.5))+
+        theme(axis.title.x=element_blank(),
+            axis.text.x=element_blank(),
+            axis.ticks.x=element_blank())}
+  else{ 
+    ggplot(data=brfss_diabetes_predict, aes(x=diabetes,y=get(input$Plot)))+
     theme_classic()+
     geom_boxplot(aes(fill=diabetes))+
-    xlab("Diabetic")+
-    labs(fill = "Diabetic")+
-    theme(plot.title = element_text(hjust = 0.5))
-})
+    labs(y="Diabetes", x= gsub("\\_", " ", input$Plot ),
+           title = paste0("Diabetes and ", gsub("\\_", " ", input$Plot )))+
+    theme(plot.title = element_text(hjust = 0.5))}
     
+}) 
 
   
 }
-
 shinyApp(ui, server)
+
+
 
